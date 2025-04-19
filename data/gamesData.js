e624632b846723696e6907471b69ba54939fd36f
Santiago Leyton <sleyton_644@unihumboldt.edu.co>
2025-04-19 00:28:14 -0500

    First Commit, added 5 first games and created initial structure with HTML, CSS, JS and folders - Santiago Leyton

---
+++ data/gamesData.js
@@ -0,0 +1,74 @@
+const games = [
+  { //EA Sports FC 24
+      id: 'Game001',
+      name: 'EA SPORTS FC™ 24',
+      description: 'FIFA 24 es el simulador de fútbol más completo y realista, ofreciéndote la oportunidad de vivir la emoción del deporte rey. Puedes jugar con los equipos y jugadores más icónicos del mundo, y competir en los torneos más prestigiosos, incluyendo ligas y copas internacionales. El juego ofrece varios modos como el Modo Carrera, donde podrás gestionar tu propio equipo o crear tu carrera como futbolista, y Ultimate Team, que te permite armar el equipo de tus sueños con jugadores de diferentes épocas. También podrás disfrutar de Clubes Pro, para formar un equipo y competir en línea, o Volta, donde se juega al fútbol callejero en escenarios urbanos. Con gráficos impresionantes y un control más fluido, FIFA 24 te permite vivir el fútbol como nunca antes, ya sea solo o acompañado.',
+      price: 184900,
+      trailer: 'https://youtu.be/-vL01jbgENE?si=y2ZfqjtY1ARCj_Fs',
+      images: [
+          'images/gamesPhotos/FC24_1.png',
+          'images/gamesPhotos/FC24_2.png',
+          'images/gamesPhotos/FC24_3.png'
+        ],
+      logo: 'images/gamesIcons/FC24.png',
+      tags: ['Deportes', 'Competitivo', 'Multijugador', 'Solitario', 'EA Sports', 'Fútbol']
+  },
+  { //F1 24
+    id: 'Game002',
+    name: 'F1 24',
+    description: 'F1 24 es el simulador oficial del Campeonato Mundial de Fórmula 1 que ofrece una experiencia de conducción realista, intensa y llena de emoción. Los jugadores pueden competir con todos los pilotos, escuderías y circuitos de la temporada 2024, viviendo cada carrera con un nivel de detalle impresionante. Con gráficos de nueva generación, físicas refinadas e inteligencia artificial avanzada, cada maniobra, cambio de clima y decisión estratégica se siente auténtica. El juego cuenta con modos como Carrera, multijugador online y pantalla dividida, lo que permite disfrutarlo en solitario o con otros. Ya sea creando tu propio piloto o encarnando a un profesional de la F1, F1 24 ofrece adrenalina, precisión y velocidad en cada vuelta. Ideal para quienes buscan desafiar sus reflejos y dominar las pistas como verdaderos campeones.',
+    price: 279900,
+    trailer: 'https://youtu.be/NnyCWsA6KSI?si=rnPDtv3o_zk5sw1z',
+    images: [
+        'images/gamesPhotos/F1_24_1.png',
+        'images/gamesPhotos/F1_24_2.png',
+        'images/gamesPhotos/F1_24_3.png'
+        ],
+    logo: 'images/gamesIcons/F1_24.png',
+    tags: ['Deportes', 'Competitivo', 'Multijugador', 'Solitario', 'EA Sports', 'Carreras']
+  },
+  { //Left 4 Dead 2
+    id: 'Game003',
+    name: 'left 4 dead 2',
+    description: 'En Left 4 Dead 2, el mundo ha colapsado y la infección ha transformado a la mayoría de la población en violentos zombis sedientos de sangre. En medio del caos, cuatro sobrevivientes desconocidos deberán confiar el uno en el otro para atravesar escenarios devastados y luchar por su vida. Equipados con armas improvisadas, explosivos y cualquier objeto que encuentren, enfrentarán hordas interminables de infectados, incluidos enemigos especiales con habilidades mortales. Cada nivel es una carrera contra el tiempo, y la cooperación es la clave para sobrevivir. Con una ambientación opresiva, acción constante y modos multijugador cooperativos y competitivos, Left 4 Dead 2 no solo pone a prueba tu puntería, sino también tu capacidad para trabajar en equipo bajo presión.',
+    price: 26000,
+    trailer: 'https://youtu.be/dE3Hm890KWQ?si=9Nfi6_rwqKOmV1n4',
+    images: [
+        'images/gamesPhotos/Left4Dead2_1.png',
+        'images/gamesPhotos/Left4Dead2_2.png',
+        'images/gamesPhotos/Left4Dead2_3.png'
+        ],
+    logo: 'images/gamesIcons/Left4Dead2.png',
+    tags: ['Acción', 'Tiro', 'Cooperativo', 'Multijugador', 'Zombis', 'Supervivencia', 'Solitario']
+  },
+  { //Minecraft
+      id: 'Game004',
+      name: 'Minecraft (Java Edition)',
+      description: 'Minecraft es un videojuego de mundo abierto donde la creatividad y la aventura no tienen límites. Los jugadores pueden explorar paisajes infinitos generados aleatoriamente, recolectar recursos, construir estructuras, fabricar herramientas y sobrevivir en un entorno lleno de criaturas, cuevas, aldeas y dimensiones ocultas. Con distintos modos de juego como Supervivencia, Creativo, Aventura y Espectador, Minecraft se adapta a todos los estilos y edades, permitiendo jugar en solitario o en compañía de otros. Su estilo visual único, basado en bloques, y su libertad total para crear o explorar, lo convierten en una experiencia inmersiva e interminable. Ideal para quienes disfrutan construir, descubrir y vivir su propia historia en un mundo completamente personalizable.',
+      price: 145000,
+      trailer: 'https://youtu.be/MmB9b5njVbA?si=d8hbOeu9hRaANFdK',
+      images: [
+          'images/gamesPhotos/MinecraftWallpaper1.png',
+          'images/gamesPhotos/MinecraftWallpaper2.png',
+          'images/gamesPhotos/MinecraftWallpaper3.png'
+        ],
+      logo: 'images/gamesIcons/Minecraft.png',
+      tags: ['Aventura', 'Construcción', 'Supervivencia', 'Multijugador', 'Solitario']
+  },
+  { //Mortal Kombat 11 Ultimate
+    id: 'Game005',
+    name: 'Mortal Kombat 11 Ultimate',
+    description: 'Mortal Kombat 11 Ultimate lleva el legendario torneo de combate al siguiente nivel. En un universo donde los dioses antiguos y guerreros inmortales luchan por el destino de las líneas temporales, cada pelea es una batalla entre el pasado y el futuro. Esta edición definitiva reúne el juego base, la expansión Aftermath y todos los Kombat Packs, ofreciendo más de 35 personajes, entre ellos íconos como Scorpion y Sub-Zero, así como nuevos combatientes como Mileena, Rain y Rambo. Con un sistema de lucha refinado, gráficos realistas y cinemáticas que cuentan una historia intensa y envolvente, MK11 Ultimate no es solo un juego de peleas, es una experiencia completa de acción, poder y gloria. Aquí no hay segundas oportunidades: el kombate es todo o nada.',
+    price: 145000,
+    trailer: 'https://youtu.be/f8o9bysnzoQ?si=rIa-aU726l4soiJ6',
+    images: [
+        'images/gamesPhotos/MK11_1.png',
+        'images/gamesPhotos/MK11_2.png',
+        'images/gamesPhotos/MK11_3.png'
+        ],
+    logo: 'images/gamesIcons/MK11.png',
+    tags: ['Acción', 'Peleas', 'Competitivo', 'Multijugador', 'Solitario']
+  }
+]
+
+const cards = []
